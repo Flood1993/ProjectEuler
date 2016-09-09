7f8160d88ea5c3dd1077199ee1adf3447f1c7365
Guillermo <guialonun@gmail.com>
2016-09-09 21:58:22 +0200

    Starting to work on p188

---
+++ p188.py
@@ -0,0 +1,22 @@
+numbers = []
+
+n = 1777
+
+counter = 0
+
+while True:
+    if n in numbers:
+        break
+    numbers.append(n)
+    counter += 1
+    n = (n * n) % 10000000
+
+print('Repeating after', counter, 'iterations. The repeating sequence is', n) # 12503 iterations
+
+for i in range(len(numbers)):
+    if numbers[i] == n:
+        print(i)
+        break
+
+# It gets to a point in which it starts repeating over again
+print(n, numbers[3])

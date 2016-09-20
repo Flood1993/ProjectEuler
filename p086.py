9afc196905b4937a527a383e96c2117f988f7e8f
Guillermo <guialonun@gmail.com>
2016-09-20 22:44:24 +0200

    Working on p086 some more. Maybe the problem is that we are not checking for the shortest routes?

--- p086.py
+++ p086.py
@@ -16,24 +16,15 @@ already_tested = set()
 
 for i in range(2, M//2):
     for j in range(1, i):
-        #if (i+j) % 2 == 0:
-        #    continue
-        #if gcd(i, j) != 1:
-        #    continue
         p = pythTriplet(i, j)
-        #inverted = False
+
         if p[0] > p[1]: # p2 is guaranteed not to be smaller than p1/p2
             p[0], p[1] = p[1], p[0]
             #inverted = True
         p0, p1, p2 = p[0], p[1], p[2]
-        while max(p[0], p[1], p[2]) <= M:
+        while max(p[0], p[1]) <= M:
             if not (p[0], p[1], p[2]) in already_tested:
-                #if inverted:
-                #    total_sols += p[1] - 1
-                #else:
-                #    total_sols += p[0] - 1
                 total_sols += p[0] - 1
-
                 already_tested.add((p[0], p[1], p[2]))
             p[0] += p0
             p[1] += p1

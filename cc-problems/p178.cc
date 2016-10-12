20f3630554a544163f1fa2f8237e24a0881f5c62
Guillermo Alonso <guialonun@gmail.com>
2016-10-12 13:47:36 +0200

    Working on problem p178

---
+++ cc-problems/p178.cc
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DIGITS_LIMIT 10
+
+long long step_pand(int last_dig, bool zero, bool nine, int cur_digits, int *memo) {
+    long long tmp = 0;
+    
+    if (cur_digits > DIGITS_LIMIT) {
+        return 0;
+    }
+    
+    int hash = 0;
+    if (zero) {
+        hash += 10000;
+    }
+    if (nine) {
+        hash += 1000;
+    }
+    hash += last_dig * 100;
+    hash += cur_digits;
+
+    if (memo[hash] != -1) {
+        return memo[hash];
+    }
+
+    if (zero && nine) { // Number is pandigital
+        tmp = 1;
+
+        if (last_dig == 0) {
+            tmp += step_pand(1, true, nine, cur_digits + 1, memo);
+        } else if (last_dig == 9) {
+            tmp += step_pand(8, zero, true, cur_digits + 1, memo);
+        } else {
+            tmp += step_pand(last_dig - 1, zero, nine, cur_digits + 1, memo);
+            tmp += step_pand(last_dig + 1, zero, nine, cur_digits + 1, memo);
+        }
+
+        memo[hash] = tmp;
+        return tmp; // Is this return correct?
+    }
+    
+    if (last_dig == 0) {
+        tmp += step_pand(1, true, nine, cur_digits + 1, memo);
+    } else if (last_dig == 9) {
+        tmp += step_pand(8, zero, true, cur_digits + 1, memo);
+    } else {
+        tmp += step_pand(last_dig - 1, zero, nine, cur_digits + 1, memo);
+        tmp += step_pand(last_dig + 1, zero, nine, cur_digits + 1, memo);
+    }
+
+    memo[hash] = 0;
+    return 0;
+}
+
+int main() {
+    long long res = 0;
+
+    int size_to_alloc = 12000;
+    int *memo = (int *) malloc(size_to_alloc * sizeof(int));
+    memset(memo, -1, size_to_alloc * sizeof(int));
+
+    for (int i = 1; i < 9; i++) {
+        res += step_pand(i, false, false, 1, memo);
+    }
+    res += step_pand(9, false, true,  1, memo);
+
+    printf("%ld\n", res);
+
+    free(memo);
+    return 0;
+}

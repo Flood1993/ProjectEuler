e4c04c88aed7a7491f6135211217b2486661eebc
Guillermo Alonso <guialonun@gmail.com>
2016-10-21 02:18:29 +0200

    Not even memoization seems to work... We must work the problem inside out

--- p343.py
+++ p343.py
@@ -2,9 +2,20 @@ from functions import gcd
 
 LIMIT = 100
 
+memo = {}
+
 def f(y, x=1):
+    keys = set()
+    found = False
     while y > 1:
+        if (x, y) in memo:
+            found = True
+            val = memo[(x, y)]
+            break
+        keys.add((x, y))
+
         _gcd = gcd(x, y)
+
         if _gcd != 1:
             x //= _gcd
             y //= _gcd
@@ -12,13 +23,30 @@ def f(y, x=1):
             x += 1
             y -= 1
 
+    keys.add((x, y))
+
+    for k in keys:
+        if k not in memo:
+            if found:
+                memo[k] = val
+            else:
+                memo[k] = x
+
+    if found:
+        return val
+
     return x
 
-assert f(20) == 6
+assert f(20) == 6, "ERROR: Basic case not working"
 
-res = 0
+def F(n):
+    res = 0
 
-for i in range(1, LIMIT + 1):
-    res += f(i**3)
+    for i in range(1, n + 1):
+        res += f(i**3)
 
-print(res)
+    return res
+
+assert F(100) == 118937, "ERROR: Example not working"
+
+print(F(1000))

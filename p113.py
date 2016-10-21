44dca98ff77064ebab6e3b2ea9eca6136695d575
Guillermo Alonso <guialonun@gmail.com>
2016-10-22 00:35:12 +0200

    Solved 113

--- p113.py
+++ p113.py
@@ -1,46 +1,66 @@
-LIMIT = 3 # Maximum digits of the number. If set to 3, we will calculate the numbers of up to 3 digits.
+LIMIT = 100
 
-# Generate all increasing numbers
-# Generate all drecreasing numbers
-# Count constant numbers: 111, 11, 1, 2222222, 22222, 22...
-# Result = increasing + decreasing - constant
+# x represents the increasing values
+x = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for i in range(LIMIT)]
 
-increasing = [-1]*1000
-decreasing = [-1]*1000
+for i in range(len(x[0])):
+	x[0][i] = 1
+x[0][0] = 0
 
-def get_increasing(last_digit, digits=1):
-    tmp = 0
+for i in range(1, len(x)):
+	for j in range(len(x[i])):
+		for k in range(j + 1):
+			x[i][j] += x[i-1][k]
 
-    hash = last_digit*100 + (digits - 1)
+# y represents the decreasing values
+y = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for i in range(LIMIT)]
 
-    if increasing[hash] != -1:
-        return increasing[hash]
+for i in range(len(y[0])):
+	y[0][i] = 1
+y[0][0] = 0
 
-    if digits == LIMIT:
-        increasing[hash] = 1
-        return increasing[hash]
+for i in range(1, len(y)):
+	for j in range(len(y[i])):
+		for k in range(j, len(y[i])):
+			y[i][j] += y[i-1][k]
 
-    for i in range(last_digit, 10):
-        tmp += get_increasing(i, digits + 1)
+if LIMIT <= 6:
+	print(x)
+	print(y)
 
-    return tmp
+"""
+Increasing numbers of 2 digit having 0 as the last digit:
+"""
 
-def get_all_increasing():
-    res = 0
-    for i in range(10):
-        res += get_increasing(i)
-    return res
+total = 0
+for i in range(len(x)):
+	total += sum(x[i])
+	total += sum(y[i])
+print(total - LIMIT*9) # TODO: Substract values which are at the same time increasing and decreasing, that is, numbers with all equal digits.
 
-inc = get_all_increasing()
-inc_dec = 2*inc
-non_bouncy = inc_dec - (1 + 9*LIMIT)
-print(inc)
-print(non_bouncy)
+"""
+If len was 3, we should take out:
+1, 11, 111
+2, 22, 222
+3, 33, 333
+4, 44, 444
+5, 55, 555
+6, 66, 666
+7, 77, 777
+8, 88, 888
+9, 99, 999
 
-tmp_res = 0
-for a in range(10):
-    for b in range(10):
-        for c in range(10):
-                if b > a and c < b or b < a and c > b:
-                    tmp_res += 1
-print(tmp_res)
+Number of increasing numbers of 2 digits ending with
+0 - 0
+1 - 1 (11)
+2 - 2 (12, 22)
+3 - 3 (13, 23, 33)
+4 - 4 (14, 24, 34, 44)
+...
+
+Number of increasing numbers of 3 digits ending with
+0 - 0
+1 - 1 (111)
+2 - 3 (112, 122, 222)
+3 - 6 (113, 123, 133, 223, 233, 333)
+"""

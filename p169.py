3dbac807d2158d3841ef09a0cdbb6ed4ce140721
Guillermo Alonso <guialonun@gmail.com>
2020-05-30 12:55:41 +0300

    Add solution for problem 169

--- p169.py
+++ p169.py
@@ -1,13 +1,43 @@
-import math
+# 2**84 > 10**25. We need 86 for counting the power of 0 and having 1 spare due to the way we calculate it, i.e., we use powers[x+1].
+N = 86
 
-N = 10**25
+powers = [2**i for i in range(0, N)]
+memoization = {}
 
-limit = int(math.log(N, 2))
+def f(x, y):
+    """
+    Params
+    ======
+        x: Powers available. We assume we have available always 2 of the powers. So if x = 4, we have 2 of each: 2**0, 2**1, 2**2, and 2**3.
+        y: Target number which we want to calculate with the available powers.
+    """
+    key = (x, y)
+    if (key in memoization):
+        return memoization[key]
 
-pows = []
+    if y < 0:
+        return 0
+    if y == 0:
+        return 1
+    if x == 0:
+        return 0
 
-for i in range(0, limit + 1):
-    pows.append(2**i)
-    pows.append(2**i)
+    if (y > 2*(powers[x+1] - 1)):
+        return 0
 
-print(pows, len(pows)) # 168 elements
+    res = 0
+    for i in range(0, x):
+        res = res + f(i, y - powers[i]) + f(i, y - 2*powers[i])
+
+    memoization[key] = res
+
+    return res
+
+def F(x, y):
+    result = f(x, y)
+    print(f"f({x}, {y}):", result)
+    return result
+
+assert F(4, 10) == 5
+
+F(84, 10**25)

cf1aabe1b17a799ed69e17cea55366c34cb7f938
Guillermo Alonso <guialonun@gmail.com>
2020-06-16 19:43:50 +0300

    Bruteforce problem 96

--- p096.py
+++ p096.py
@@ -1,38 +1,90 @@
-s = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
+def solve(s):
+    if completed(s):
+        return int(s[0:3])
 
-assert len(s) == 81
+    if violates_rules(s):
+        return False
 
-L = [[] for i in range(81)]
+    return brute_force(s)
 
-print(L)
+def violates_rules(s):
+    return violates_rows(s) or violates_columns(s) or violates_squares(s)
 
-for i in range(len(s)):
-    if s[i] != '0':
-        L[i] = int(s[i])
+def violates_rows(s):
+    for i in range(9):
+        if violates_row(s, i):
+            return True
 
-def get_pos(p):
-    """
-    Returns the pos of p in format row, column.
-    """
-    return (p//9, p%9)
+    return False
 
-def check_candidates(s, p):
-    """
-    Note that the position p in the real sudoku is (p//9, p%9),
-    being 0,0 (s[0]) the topleft most cell and 8,8 (s[80]) the
-    bottomright cell.
-    """
-    candidates = [True for i in range(10)] # Each position represents whether the i number can be placed there
-    candidates[0] = False
+def violates_row(s, i):
+    row = s[9*i : 9*(i+1)]
 
-    # Check rows.
-    cur_pos = get_pos(s, p)
-    for i in range(81):
-        if get_pos(s, i)[0] == cur_pos[0] and not isinstance(s[i], List):
-            candidates[s[i]] = False
+    return repeated_numbers(row)
+
+def violates_columns(s):
+    for i in range(9):
+        if violates_column(s, i):
+            return True
+
+    return False
+
+def violates_column(s, i):
+    column = s[i : 81 : 9]
+
+    return repeated_numbers(column)
+
+def violates_squares(s):
+    squares_top_left = [0, 3, 6, 27, 30, 33, 54, 57, 60]
     
-    # Check columns.
+    for top_left in squares_top_left:
+        if violates_square_with_top_left(s, top_left):
+            return True
 
-    # Check square.
+    return False
 
-print(L)
+def violates_square_with_top_left(s, top_left):
+    sq = s[top_left:top_left + 3] + s[top_left + 9:top_left + 12] + s[top_left + 18:top_left + 21]
+
+    return repeated_numbers(sq)
+
+def repeated_numbers(substring):
+    preprocessed_substring = substring.replace("0", "")
+    length = len(preprocessed_substring)
+
+    if length > 0:
+        return length != len(set(preprocessed_substring))
+
+    return False
+
+def completed(s):
+    if s.find('0') == -1 and violates_rules(s):
+        return False
+
+    return s.find('0') == -1 and not violates_rules(s)
+
+def brute_force(s):
+    first_zero = s.find('0')
+    s_list = list(s)
+    
+    if first_zero == -1:
+        return False
+
+    for i in range(1, 10):
+        s_list[first_zero] = str(i)
+        s_back_to_string = "".join(s_list)
+
+        result = solve(s_back_to_string)
+        if result:
+            return result
+
+    return False
+
+total = 0
+
+with open('p096_sudoku.txt', 'r') as f:
+    for sudoku in f.readlines():
+        print('solved')
+        total += solve(sudoku)
+
+print(total)
